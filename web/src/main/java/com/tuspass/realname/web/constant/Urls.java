38525bc122ec3b4e85c443cb9c0c63c9a7af76a2
lxs <lxs_shuai@163.com>
2020-02-22 13:16:49 +0800

    防疫实名乘车

--- web/src/main/java/com/tuspass/realname/web/constant/Urls.java
+++ web/src/main/java/com/tuspass/realname/web/constant/Urls.java
@@ -7,7 +7,7 @@ package com.tuspass.realname.web.constant;
  **/
 public class Urls {
 
-    public static final String USERS_ADD = "/users/add";
+    public static final String USERS_ADD = "/users/login";
 
     public static final String USERS_SELECT = "/users/";
 
